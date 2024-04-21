33db3cb5fc096469ee32e833bf1d9af49defb68d
Harshil Tomar <harshiltomar20@gmail.com>
2024-04-21 14:12:26 +0530

    release

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,18 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Harshil ka BackEnd",
-  });
+  // Get User details from frontend
+  // Validation - not empty
+  // Check if user already exists: username, email
+  // Check for images, check for avatar
+  // Upload them to Cloudinary, avatar
+  // Create user object- create entry in db
+  // Remove password and refresh token feild from Response
+  // Check for user creation
+  // Return response
+
+  const { fullName, email, username, password } = req.body;
+  console.log("email", email);
 });
 
 export { registerUser };
